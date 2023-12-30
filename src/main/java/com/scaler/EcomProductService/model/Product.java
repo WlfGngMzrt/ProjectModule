ad3bedbf3f1233aacb9fe5aeaaaa26ce9ccddd6e
syandan98@gmail.com <syandan98@gmail.com>
2023-12-30 19:16:04 +0530

    Notes - Commit

--- src/main/java/com/scaler/EcomProductService/model/Product.java
+++ src/main/java/com/scaler/EcomProductService/model/Product.java
@@ -1,12 +1,27 @@
 package com.scaler.EcomProductService.model;
 
-public class Product {
-    private int id;
+import com.fasterxml.jackson.databind.ser.Serializers;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Product extends BaseModel {
+
     private String title;
     private double price;
-    private String category;
+    @ManyToOne
+    private Category category;
     private String description;
     private String image;
 
+
+    /*
+    Product  - Category : M : 1
+
+     */
 
 }
